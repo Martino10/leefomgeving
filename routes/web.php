c9a2cd283173cdb7acabbe0cf26c86a7bf6e7e4e
Martino10 <martin.krikke10@gmail.com>
2022-04-11 14:27:13 +0200

    web.php weer gefixt

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::middleware(['auth'])->group(function () {
+    // Routes waar je ingelogd voor moet zijn
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::get('/data', [\App\Http\Controllers\DataController::class, 'index'])->name('data');
+
+
 
 require __DIR__.'/auth.php';

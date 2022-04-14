8a01ed5d406d5639be1f17ad4767d5220b2c2490
Yeic554 <71875612+Yeic554@users.noreply.github.com>
2022-04-14 13:35:40 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,10 +1,13 @@
 import mysql.connector
 import serial
+import time
 from datetime import datetime
 from matplotlib import pyplot as plt
 import matplotlib
 
 port = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=3.0)
+dataString = "0, 0, 0, 0, 0"
+dataList = dataString.split(",") #splitten en list van maken
 
 mydb = mysql.connector.connect(
     host="localhost",
@@ -13,6 +16,8 @@ mydb = mysql.connector.connect(
     database="leefomgeving"
 )
 
+mycursor = mydb.cursor()
+
 times = []
 gasvalues = []
 tempvalues = []
@@ -20,52 +25,68 @@ humidvalues = []
 lightvalues = []
 
 while True:
-    if port.readline() != "":
-       dataString = port.readline() #alles van serial inladen in iets
+    if len(dataList) == 5:
+        decoding = port.readline() #alles van serial inladen in iets
+        dataString = decoding.decode("utf-8")
+        print(dataString)
         dataList = dataString.split(",") #splitten en list van maken
-        sqlgas = dataArray[0] #specificeerd welke index uit de lijst gepakt moet worden.
-        sqltemp = dataArray[1]
-        sqlhumid = dataArray[2]
-        sqlsound = dataArray[3]
-        if sqlsound >= 1:
-            geluidsoverlast = Ja
+        sqlgas = dataList[0] #specificeerd welke index uit de lijst gepakt moet worden.
+        sqltemp = dataList[1]
+        sqlhumid = dataList[2]
+        sqlsound = dataList[3]
+        print(sqlsound)
+        if sqlsound == "1":
+            geluidsoverlast = "Ja"
         else:
-            geluidsoverlast = Nee
-        ldrValue = dataArray[4]
+            geluidsoverlast = "Nee"
+        ldrValue = dataList[4]
 
         # berekening algemene kwaliteitsscore
-        gasscore = min(100 - ((sqlgas-600)/100 * 5),100)
-        tempscore = 100 - (abs((sqltemp - 20) * 7))
-        humidscore = 100 - (abs(sqlhumid - 45) * 2)
-        soundscore = abs(sqlsound * 100 - 100)
-        lightscore = 100 - (abs(ldrValue - 700) / 10)
+        gasscore = min(100 - ((int(sqlgas)-600)/100 * 5),100)
+        tempscore = 100 - (abs((int(sqltemp) - 20) * 7))
+        humidscore = 100 - (abs(int(sqlhumid) - 45) * 2)
+        soundscore = abs(int(sqlsound) * 100 - 100)
+        lightscore = 100 - (abs(int(ldrValue) - 700) / 10)
         scores = [gasscore, tempscore, humidscore, soundscore, lightscore]
 
         sqlscore = sum(scores) / len(scores)
 
+        location = 1
+
+        sql = """INSERT INTO gas (location_id) VALUES (location)"""
         sql = """INSERT INTO gas (value) VALUES (%s)""" #namen van 
         mycursor.execute(sql, (sqlgas,)) #execute de sql statement met data uit lijst. %s is waarde van sqlgas.
+
+        sql = """INSERT INTO temperatuur (location_id) VALUES (location)"""
         sql = """INSERT INTO temperatuur (value) VALUES (%s)"""
         mycursor.execute(sql, (sqltemp,))
+
+        sql = """INSERT INTO luchtvochtigheid (location_id) VALUES (location)"""
         sql = """INSERT INTO luchtvochtigheid (value) VALUES (%s)"""
         mycursor.execute(sql, (sqlhumid,))
+
+        sql = """INSERT INTO geluid (location_id) VALUES (location)"""
         sql = """INSERT INTO geluid (value) VALUES (%s)"""
         mycursor.execute(sql, (geluidsoverlast,))
+
+        sql = """INSERT INTO ldr (location_id) VALUES (location)"""
         sql = """INSERT INTO ldr (value) VALUES (%s)"""
         mycursor.execute(sql, (ldrValue,))
+
+        sql = """INSERT INTO qualityscore (location_id) VALUES (location)"""
         sql = """INSERT INTO qualityscore (value) VALUES (%s)"""
         mycursor.execute(sql, (sqlscore,))
-        
+        mydb.commit()
         #tabelaanpassingen zijn altijd nog mogelijk
 
         # ---------------
         # grafieken maken
-        
+
         datetime = datetime.now() # haalt datum en tijd op
         date = datetime.strftime('%d-%m-%Y')
-        time = datetime.strftime('%H:%M')
-        times.append(time)
-        time_objects = [datetime.datetime.strptime(t, "%H:%M") for t in times]
+        time_str = datetime.strftime('%H:%M')
+        times.append(time_str)
+        time_objects = [datetime.strptime(t, "%H:%M") for t in times]
 
         # gas
         gasvalues.append(sqlgas)
@@ -98,3 +119,4 @@ while True:
         plt.ylabel('Brightness (Lux)')
         plt.title('Light')
         plt.savefig('public/img/lightgraph.png', bbox_inches='tight')
+        time.sleep(60)
